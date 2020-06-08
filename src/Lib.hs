c31e923f3331300cf8456826026cc167c6b64dfb
Ignacio Garcia <nachog.2000@hotmail.com>
2020-06-08 20:35:09 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,38 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+-- Punto 1)
+
+type Enfermedad = String
+
+data Raton = UnRaton {
+    nombre :: String,
+    edad :: Float,
+    peso :: Float,
+    enfermedades :: [Enfermedad]
+}deriving (Show)
+
+cerebro :: Raton
+cerebro = UnRaton {
+    nombre = "Cerebro",
+    edad = 9,
+    peso = 0.2,
+    enfermedades = ["brucelosis","sarampion","tuberculosis"]
+}
+
+bicenterrata :: Raton
+bicenterrata = UnRaton {
+    nombre = "Bicenterrata",
+    edad = 256,
+    peso = 0.2,
+    enfermedades = []
+}
+
+huesudo :: Raton
+huesudo = UnRaton {
+    nombre = "Huesudo",
+    edad = 4,
+    peso = 10,
+    enfermedades = ["alta obesidad", "sinusitis"]
+}
